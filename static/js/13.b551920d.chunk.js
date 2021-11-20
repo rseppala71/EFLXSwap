a772a8b580f08dd76e16ba8f4fe7c2753ee487e4
connectshani <mushani101@gmail.com>
2021-11-20 18:29:21 +0500

    asd

---
+++ static/js/13.b551920d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[13],{1125:function(n,c){},1175:function(n,c){},1177:function(n,c){},1210:function(n,c){},1228:function(n,c){},1230:function(n,c){}}]);
+//# sourceMappingURL=13.b551920d.chunk.js.map
